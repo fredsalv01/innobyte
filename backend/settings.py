15071dcbd9c587d2fa7ef8322ea353899cf54acd
fredalbert37 <fredalbert37@outlook.com>
2022-09-11 17:28:14 -0500

    add cors headers

--- backend/settings.py
+++ backend/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
+    'corsheaders'
 ]
 
 REST_FRAMEWORK = {
